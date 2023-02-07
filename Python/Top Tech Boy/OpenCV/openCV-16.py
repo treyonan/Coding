c7859ba22954f9b53e4b8a43b965cbf8dad189e1
treyonan <trey_onan@yahoo.com>
2023-02-06 22:46:42 -0600

    amend

--- Python/Top Tech Boy/OpenCV/openCV-16.py
+++ Python/Top Tech Boy/OpenCV/openCV-16.py
@@ -6,48 +6,25 @@
 
 import cv2
 import numpy as np
-print(cv2.__version__)
-evt=0
-xVal=0
-yVal=0
-def mouseClick(event, xPos, yPos, flags, params):
-    global evt
-    global xVal
-    global yVal
-    if event==cv2.EVENT_LBUTTONDOWN:
-        print(event)
-        xVal=xPos
-        yVal=yPos
-        evt=event
-    if event==cv2.EVENT_RBUTTONUP:
-        evt=event
-        print(event)
 
+x=np.zeros([256,720,3],dtype=np.uint8)
+for row in range(0,256,1):
+    for col in range(0,720,1):
+        x[row,col]=(int(col/4),row,255)
+x=cv2.cvtColor(x,cv2.COLOR_HSV2BGR)
 
-width=640
-height=360
-cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
-cam.set(cv2.CAP_PROP_FPS, 30)
-cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
-cv2.namedWindow('my WEBcam')
-cv2.setMouseCallback('my WEBcam',mouseClick)
+y=np.zeros([256,720,3],dtype=np.uint8)
+for row in range(0,256,1):
+    for col in range(0,720,1):
+        y[row,col]=(int(col/4),255,row)
+y=cv2.cvtColor(y,cv2.COLOR_HSV2BGR)
+
 while True:
-    ignore,  frame = cam.read()
-    if evt==1:
-        x=np.zeros([250,250,3],dtype=np.uint8)
-        y=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        clr=y[yVal][xVal]
-        print(clr)
-        x[:,:]=clr
-        cv2.putText(x, str(clr),(0,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),1)
-        cv2.imshow('Color Picker', x)
-        cv2.moveWindow('Color Picker', width, 0)
-        evt=0
-    cv2.imshow('my WEBcam', frame)
-    cv2.moveWindow('my WEBcam',0,0)
-    if cv2.waitKey(1) & 0xff ==ord('q'):
-        cv2.destroyAllWindows()
+    cv2.imshow('my HSV',x)
+    cv2.moveWindow('my HSV',0,0)
+    cv2.imshow('my HSV2',y)
+    cv2.moveWindow('my HSV2',0,300)
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
-cam.release()
+        cv2.destroyAllWindows()
+
