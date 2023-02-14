f011c4a0f97b5d2238bd63907c5247c7c2863cda
treyonan <trey_onan@yahoo.com>
2023-02-13 20:36:31 -0600

    amend

--- Python/Top Tech Boy/OpenCV/openCV-18.py
+++ Python/Top Tech Boy/OpenCV/openCV-18.py
@@ -37,6 +37,16 @@ def onTrack6(val):
     valHigh=val
     print('Val High',valHigh)
 
+def onTrack7(val):
+    global hueLow2
+    hueLow2=val
+    print('Hue Low 2',hueLow2)
+
+def onTrack8(val):
+    global hueHigh2
+    hueHigh2=val
+    print('Hue High 2',hueHigh2)
+
 width=640
 height=360
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -45,7 +55,8 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
 
-cv2.namedWindow('myTracker')
+cv2.namedWindow('myTracker',cv2.WINDOW_NORMAL)
+cv2.resizeWindow('myTracker',350,350)
 cv2.moveWindow('myTracker',width,0)
 
 hueLow=10
@@ -57,6 +68,8 @@ valHigh=250
 
 cv2.createTrackbar('Hue Low','myTracker',10,179,onTrack1)
 cv2.createTrackbar('Hue High','myTracker',20,179,onTrack2)
+cv2.createTrackbar('Hue Low 2','myTracker',10,179,onTrack7)
+cv2.createTrackbar('Hue High 2','myTracker',20,179,onTrack8)
 cv2.createTrackbar('Sat Low','myTracker',10,255,onTrack3)
 cv2.createTrackbar('Sat High','myTracker',250,255,onTrack4)
 cv2.createTrackbar('Val Low','myTracker',10,255,onTrack5)
@@ -68,15 +81,29 @@ while True:
     frameHSV=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     lowerBound=np.array([hueLow,satLow,valLow])
     upperBound=np.array([hueHigh,satHigh,valHigh])
+
+    lowerBound2=np.array([hueLow2,satLow,valLow])
+    upperBound2=np.array([hueHigh2,satHigh,valHigh])
+
     myMask=cv2.inRange(frameHSV,lowerBound,upperBound)
+    myMask2=cv2.inRange(frameHSV,lowerBound2,upperBound2)
+
+    #myMaskComp=myMask | myMask2
+    myMaskComp=cv2.add(myMask,myMask2)
+
     #myMask=cv2.bitwise_not(myMask)
-    myObject=cv2.bitwise_and(frame,frame,mask=myMask)
+    myObject=cv2.bitwise_and(frame,frame,mask=myMaskComp)
     myObjectSmall=cv2.resize(myObject,(int(width/2),int(height/2)))
     cv2.imshow('My Object', myObjectSmall) 
     cv2.moveWindow('My Object', int(width/2),int(height)) 
     myMaskSmall=cv2.resize(myMask,(int(width/2),int(height/2)))
     cv2.imshow('My Mask', myMaskSmall)
     cv2.moveWindow('My Mask',0,height)
+
+    myMaskSmall2=cv2.resize(myMask2,(int(width/2),int(height/2)))
+    cv2.imshow('My Mask 2', myMaskSmall2)
+    cv2.moveWindow('My Mask 2',0,height+int(height/2))
+
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
