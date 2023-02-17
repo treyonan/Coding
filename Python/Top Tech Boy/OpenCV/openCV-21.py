eb35e1dfbe0ed73fe1d12f198736d9e1d97511ac
treyonan <trey_onan@yahoo.com>
2023-02-16 22:50:07 -0600

    amend

--- Python/Top Tech Boy/OpenCV/openCV-21.py
+++ Python/Top Tech Boy/OpenCV/openCV-21.py
@@ -9,6 +9,9 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
+
+faceCascade=cv2.CascadeClassifier(r'C:\Users\trey_\OneDrive\Projects\Coding\Python\Top Tech Boy\OpenCV\haar\haarcascade_frontalface_default.xmlhaarcascade_frontalface_default.xml')
+
 while True:
     ignore,  frame = cam.read()
     cv2.imshow('my WEBcam', frame)
