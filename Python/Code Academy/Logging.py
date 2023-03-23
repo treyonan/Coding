dbf9bd3c48f870aa939c530f9110b7a79c7e4894
treyonan <trey_onan@yahoo.com>
2023-03-22 21:32:42 -0500

    amend

--- Python/Code Academy/Logging.py
+++ Python/Code Academy/Logging.py
@@ -111,3 +111,28 @@ def division():
 result = division()
 if result == None:
   logger.warning("The result value is None!")
+
+# Using basicConfig() ------------------------------------------------------------
+
+logging.basicConfig(filename='basic_config.log', level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+        
+def division():
+  logger.debug("Starting Division!")
+  try:
+    dividend = float(input("Enter the dividend: "))
+    logger.info(dividend)
+    divisor = float(input("Enter the divisor: "))
+    logger.info(divisor)
+  except SyntaxError:
+    logger.log(logging.CRITICAL, "No dividend or divisor value entered!")
+    return
+  if divisor == 0:
+    logger.error("Attempting to divide by 0!")
+    return
+  else:
+    return dividend/divisor
+
+result = division()
+if result == None:
+  logger.warning("The result value is None!")
