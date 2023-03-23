85945dbae07abba1a535023db0c54deb36666cba
treyonan <trey_onan@yahoo.com>
2023-03-22 20:55:03 -0500

    amend

--- Python/Code Academy/Logging.py
+++ Python/Code Academy/Logging.py
@@ -47,3 +47,29 @@ def convert(from_country, to_country, amount):
 logger.debug("Current rates: {exchange_rates}".format(exchange_rates=exchange_rates))        
 currency = convert("EUR", "USD", 45)
 print(currency)
+
+# Logging to a File ----------------------------------------------------------------
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("output.log")
+logger.addHandler(file_handler)
+        
+def division():
+  logger.debug("Starting Division!")
+  try:
+    dividend = float(input("Enter the dividend: "))
+    logger.info(dividend)
+    divisor = float(input("Enter the divisor: "))
+    logger.info(divisor)
+  except SyntaxError:
+    logger.log(logging.CRITICAL, "No dividend or divisor value entered!")
+    return
+  if divisor == 0:
+    logger.error("Attempting to divide by 0!")
+    return
+  else:
+      return dividend/divisor
+result = division()
+if result == None:
+ logger.warning("The result value is None!")
