5f5b50d1f4bf0791b356c36d992ee09ce0768b93
treyonan <trey_onan@yahoo.com>
2023-04-26 21:48:29 -0500

    amend

--- Python/Code Academy/Data Structures and Algorithms/Hash_Maps.py
+++ Python/Code Academy/Data Structures and Algorithms/Hash_Maps.py
@@ -3,21 +3,73 @@ class HashMap:
     self.array_size = array_size
     self.array = [None for item in range(array_size)]
 
-  def hash(self, key):
+  def hash(self, key, count_collisions=0):
     key_bytes = key.encode()
     hash_code = sum(key_bytes)
-    return hash_code
+    return hash_code + count_collisions
 
   def compressor(self, hash_code):
     return hash_code % self.array_size
 
   def assign(self, key, value):
     array_index = self.compressor(self.hash(key))
-    self.array[array_index] = value
+    current_array_value = self.array[array_index]
+
+    if current_array_value is None:
+      self.array[array_index] = [key, value]
+      return
+
+    if current_array_value[0] == key:
+      self.array[array_index] = [key, value]
+      return
+
+    # Collision!
+
+    number_collisions = 1
+
+    while(current_array_value[0] != key):
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
+
+      if current_array_value is None:
+        self.array[new_array_index] = [key, value]
+        return
+
+      if current_array_value[0] == key:
+        self.array[new_array_index] = [key, value]
+        return
+
+      number_collisions += 1
+
+    return
 
   def retrieve(self, key):
     array_index = self.compressor(self.hash(key))
-    return self.array[array_index]
+    possible_return_value = self.array[array_index]
+
+    if possible_return_value is None:
+      return None
+
+    if possible_return_value[0] == key:
+      return possible_return_value[1]
+
+    retrieval_collisions = 1
+
+    while (possible_return_value != key):
+      new_hash_code = self.hash(key, retrieval_collisions)
+      retrieving_array_index = self.compressor(new_hash_code)
+      possible_return_value = self.array[retrieving_array_index]
+
+      if possible_return_value is None:
+        return None
+
+      if possible_return_value[0] == key:
+        return possible_return_value[1]
+
+      retrieval_collisions += 1
+
+    return
 
 hash_map = HashMap(20)
 hash_map.assign("gneiss", "metamorphic")
