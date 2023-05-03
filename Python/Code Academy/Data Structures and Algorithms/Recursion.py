0ffd0f9c903d12b022fa2e0132d6157c228d403d
treyonan <trey_onan@yahoo.com>
2023-05-03 12:59:49 -0500

    amend

--- Python/Code Academy/Data Structures and Algorithms/Recursion.py
+++ Python/Code Academy/Data Structures and Algorithms/Recursion.py
@@ -32,3 +32,35 @@ def sum_to_one(n):
 
 # uncomment when you're ready to test
 print(sum_to_one(7))
+
+
+# factorial ------------------
+ 
+def factorial(n):
+  if n == 1:
+    return n
+  else:
+    return n * factorial(n-1)
+    
+print(factorial(12))
+
+# power set ---------------------
+
+def power_set(my_list):
+    # base case: an empty list
+    if len(my_list) == 0:
+        return [[]]
+    # recursive step: subsets without first element
+    power_set_without_first = power_set(my_list[1:])
+    # subsets with first element
+    with_first = [ [my_list[0]] + rest for rest in power_set_without_first ]
+    # return combination of the two
+    return with_first + power_set_without_first
+  
+universities = ['MIT', 'UCLA', 'Stanford', 'NYU']
+power_set_of_universities = power_set(universities)
+
+for set in power_set_of_universities:
+  print(set)
+
+
