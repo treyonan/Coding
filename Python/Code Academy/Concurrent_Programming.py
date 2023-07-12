86805345a9ce457172d57d1c6e05f4819c2f4920
Trey O'Nan <trey_onan@yahoo.com>
2023-07-11 19:54:40 -0500

    amend

--- Python/Code Academy/Concurrent_Programming.py
+++ Python/Code Academy/Concurrent_Programming.py
@@ -13,3 +13,18 @@ sequential()
 
 # The Threading Module --------------------------------------------------------
 
+import time
+import threading
+def greeting_with_sleep(string):
+  s = time.perf_counter()
+  print(string)
+  time.sleep(2)
+  print("says hello!")
+  elapsed = time.perf_counter() - s
+  print("Sequential Programming Elapsed Time: " + str(elapsed) + " seconds")
+
+greeting_with_sleep('Codecademy')
+
+t = threading.Thread(target=greeting_with_sleep, args=('Codecademy',))
+t.start()
+
