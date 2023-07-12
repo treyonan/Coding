9d4a64710fa80330c5295864a44599b9fad8e196
Trey O'Nan <trey_onan@yahoo.com>
2023-07-11 20:19:18 -0500

    amend

--- Python/Code Academy/Concurrent_Programming.py
+++ Python/Code Academy/Concurrent_Programming.py
@@ -11,7 +11,7 @@ def sequential():
 
 sequential()
 
-# The Threading Module --------------------------------------------------------
+# Multiple Threads --------------------------------------------------------
 
 import time
 import threading
@@ -32,3 +32,26 @@ def main_threading():
   print("Threading Elapsed Time: " + str(elapsed) + " seconds")
 
 main_threading()
+
+# Joining a Thread -----------------------------------------------------------------
+
+import time
+import threading
+def greeting_with_sleep(string):
+  print(string)
+  time.sleep(2)
+  print("says hello!")
+
+
+def main_threading():
+  s = time.perf_counter()
+  threads = []
+  greetings = ['Codecademy', 'Chelsea', 'Hisham', 'Ashley']
+  for i in range(len(greetings)):
+    t = threading.Thread    (target=greeting_with_sleep, args=(greetings[i],)) 
+    t.start()
+    # add append code here
+    threads.append(t)
+  # add join code here
+  for t in threads:
+    t.join()
