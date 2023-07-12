958c35ff4cf29c8fd0a339fe2240e849360e410e
Trey O'Nan <trey_onan@yahoo.com>
2023-07-11 20:09:40 -0500

    amend

--- Python/Code Academy/Concurrent_Programming.py
+++ Python/Code Academy/Concurrent_Programming.py
@@ -16,15 +16,19 @@ sequential()
 import time
 import threading
 def greeting_with_sleep(string):
-  s = time.perf_counter()
   print(string)
   time.sleep(2)
   print("says hello!")
+
+
+def main_threading():
+  s = time.perf_counter()
+  # your code goes here
+  greetings = ['Codecademy', 'Chelsea', 'Hisham', 'Ashley']
+  for i in range(len(greetings)):
+    t = threading.Thread    (target=greeting_with_sleep, args=(greetings[i],))
+    t.start()
   elapsed = time.perf_counter() - s
-  print("Sequential Programming Elapsed Time: " + str(elapsed) + " seconds")
+  print("Threading Elapsed Time: " + str(elapsed) + " seconds")
 
-greeting_with_sleep('Codecademy')
-
-t = threading.Thread(target=greeting_with_sleep, args=('Codecademy',))
-t.start()
-
+main_threading()
