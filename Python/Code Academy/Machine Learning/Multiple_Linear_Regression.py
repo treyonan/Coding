21dff859beb243823d3e3f6845b7d1bf22e663fd
Trey O'Nan <trey_onan@yahoo.com>
2023-08-07 19:56:38 -0500

    amend

--- Python/Code Academy/Machine Learning/Multiple_Linear_Regression.py
+++ Python/Code Academy/Machine Learning/Multiple_Linear_Regression.py
@@ -127,3 +127,126 @@ plt.xlabel("actual rent prices")
 plt.ylabel("predicted rent prices")
 plt.title("Actual rent vs. predicted")
 plt.show()
+
+# Multiple Linear Regression Equation --------------------------
+
+mport codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+mlr = LinearRegression()
+
+model=mlr.fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+
+# Input code here:
+print(mlr.coef_)
+
+# Correlations ---------------------------------
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+# Input code here:
+
+plt.scatter(df[['size_sqft']], df[['rent']], alpha=0.4)
+plt.scatter(df[['floor']], df[['rent']])
+plt.scatter(df[['min_to_subway']], df[['rent']])
+
+plt.show()
+
+# Evaluating the Model's Accuracy ------------------------------------
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+mlr = LinearRegression()
+
+model=mlr.fit(x_train, y_train)
+
+y_predict = mlr.predict(x_test)
+
+# Input code here:
+print("Train score:")
+print(mlr.score(x_train, y_train))
+
+print("Test score:")
+print(mlr.score(x_test, y_test))
+
+# Rebuild the model ---------------------------
+
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+streeteasy = pd.read_csv("https://raw.githubusercontent.com/sonnynomnom/Codecademy-Machine-Learning-Fundamentals/master/StreetEasy/manhattan.csv")
+
+df = pd.DataFrame(streeteasy)
+
+x = df[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway', 'floor', 'building_age_yrs', 'no_fee', 'has_roofdeck', 'has_washer_dryer', 'has_doorman', 'has_elevator', 'has_dishwasher', 'has_patio', 'has_gym']]
+
+y = df[['rent']]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, test_size = 0.2, random_state=6)
+
+lm = LinearRegression()
+
+model = lm.fit(x_train, y_train)
+
+y_predict= lm.predict(x_test)
+
+print("Train score:")
+print(lm.score(x_train, y_train))
+
+print("Test score:")
+print(lm.score(x_test, y_test))
+
+plt.scatter(y_test, y_predict, alpha=0.4)
+plt.plot(range(20000), range(20000))
+
+plt.xlabel("Prices: $Y_i$")
+plt.ylabel("Predicted prices: $\hat{Y}_i$")
+plt.title("Actual Rent vs Predicted Rent")
+
+plt.show()
+
