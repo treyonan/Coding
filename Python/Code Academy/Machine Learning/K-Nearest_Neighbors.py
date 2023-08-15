82eaad47fd87e9c0c37a9fd6f9480f98885d8463
treyonan <trey_onan@yahoo.com>
2023-08-14 23:31:41 -0500

    amend

--- Python/Code Academy/Machine Learning/K-Nearest_Neighbors.py
+++ Python/Code Academy/Machine Learning/K-Nearest_Neighbors.py
@@ -28,3 +28,44 @@ def distance(movie1, movie2):
 
 print(distance(star_wars, raiders))
 print(distance(star_wars, mean_girls))
+
+# Normalization ----------------
+
+release_dates = [1897, 1998, 2000, 1948, 1962, 1950, 1975, 1960, 2017, 1937, 1968, 1996, 1944, 1891, 1995, 1948, 2011, 1965, 1891, 1978]
+
+def min_max_normalize(lst):
+  minimum = min(lst)
+  maximum = max(lst)
+  normalized = [(i-minimum)/(maximum-minimum) for i in lst]
+  return normalized
+
+print(min_max_normalize(release_dates))
+
+# Finding the Nearest Neighbors --------------------------------------
+
+from movies import movie_dataset, movie_labels
+
+#print(movie_dataset['Bruce Almighty'])
+#print(movie_labels['Bruce Almighty'])
+
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknown, dataset, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  return neighbors
+  
+print(classify([.4, .2, .9], movie_dataset, 5))
