e728b74ca1677fed28ccaa5872d6d56f5bc9a3a9
treyonan <trey_onan@yahoo.com>
2023-08-15 22:21:21 -0500

    amend

--- Python/Code Academy/Machine Learning/K-Nearest_Neighbors.py
+++ Python/Code Academy/Machine Learning/K-Nearest_Neighbors.py
@@ -69,3 +69,189 @@ def classify(unknown, dataset, k):
   return neighbors
   
 print(classify([.4, .2, .9], movie_dataset, 5))
+
+# Count Neighbors ------------------------------------
+
+from movies import movie_dataset, movie_labels
+
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknown, dataset, labels, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  num_good = 0
+  num_bad = 0
+  for neighbor in neighbors:
+    title = neighbor[1]
+    if labels[title] == 0:
+      num_bad += 1
+    elif labels[title] == 1:
+      num_good += 1
+  if num_good > num_bad:
+    return 1
+  else:
+    return 0
+
+print(classify([.4, .2, .9], movie_dataset, movie_labels, 5))
+
+# Classify Your Favorite Movie -----------------------------
+
+from movies import movie_dataset, movie_labels, normalize_point
+
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknown, dataset, labels, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  num_good = 0
+  num_bad = 0
+  for neighbor in neighbors:
+    title = neighbor[1]
+    if labels[title] == 0:
+      num_bad += 1
+    elif labels[title] == 1:
+      num_good += 1
+  if num_good > num_bad:
+    return 1
+  else:
+    return 0
+  
+print("Call Me By Your Name" in movie_dataset)
+my_movie = [3500000, 132, 2017]
+normalized_my_movie = normalize_point(my_movie)
+print(normalized_my_movie)
+print(classify(normalized_my_movie, movie_dataset, movie_labels, 5))
+
+# Training and Validation Sets ----------------
+
+from movies import training_set, training_labels, validation_set, validation_labels
+
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknown, dataset, labels, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  num_good = 0
+  num_bad = 0
+  for neighbor in neighbors:
+    title = neighbor[1]
+    if labels[title] == 0:
+      num_bad += 1
+    elif labels[title] == 1:
+      num_good += 1
+  if num_good > num_bad:
+    return 1
+  else:
+    return 0
+
+#print(validation_set["Bee Movie"])
+#print(validation_labels["Bee Movie"])
+
+guess = classify(validation_set["Bee Movie"], training_set, training_labels, 5)
+print(guess)
+
+if (guess == validation_labels["Bee Movie"]):
+  print("Correct!")
+else:
+  print("Wrong!")
+
+# Choosing K ----------------------------------------------
+
+from movies import training_set, training_labels, validation_set, validation_labels
+
+def distance(movie1, movie2):
+  squared_difference = 0
+  for i in range(len(movie1)):
+    squared_difference += (movie1[i] - movie2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknown, dataset, labels, k):
+  distances = []
+  #Looping through all points in the dataset
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    #Adding the distance and point associated with that distance
+    distances.append([distance_to_point, title])
+  distances.sort()
+  #Taking only the k closest points
+  neighbors = distances[0:k]
+  num_good = 0
+  num_bad = 0
+  for neighbor in neighbors:
+    title = neighbor[1]
+    if labels[title] == 0:
+      num_bad += 1
+    elif labels[title] == 1:
+      num_good += 1
+  if num_good > num_bad:
+    return 1
+  else:
+    return 0
+
+def find_validation_accuracy(training_set, training_labels, validation_set, validation_labels, k):
+  num_correct = 0.0
+  for title in validation_set:
+    guess = classify(validation_set[title], training_set, training_labels, k)
+    if (guess == validation_labels[title]):
+      num_correct += 1  
+  return num_correct / len(validation_set)
+
+print(find_validation_accuracy(training_set, training_labels, validation_set, validation_labels, 3))
+
+# Using sklearn -----------------------------------------------
+
+from movies import movie_dataset, labels
+from sklearn.neighbors import KNeighborsClassifier
+
+classifier = KNeighborsClassifier(n_neighbors = 5)
+
+classifier.fit(movie_dataset, labels)
+
+unknown = [
+  [.45, .2, .5],
+  [.25, .8, .9],
+  [.1, .1, .9]
+]
+
+print(classifier.predict(unknown))
