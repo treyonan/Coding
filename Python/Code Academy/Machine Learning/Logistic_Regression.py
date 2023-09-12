fb4244f97f0f8a711bceee82dd81c3d0854f78ac
treyonan <102309885+treyonan@users.noreply.github.com>
2023-09-11 19:40:54 -0500

    amend

--- Python/Code Academy/Machine Learning/Logistic_Regression.py
+++ Python/Code Academy/Machine Learning/Logistic_Regression.py
@@ -62,3 +62,28 @@ odds_on_time = p/(1-p)
 # Calculate log_odds_on_time
 log_odds_on_time = np.log(odds_on_time)
 
+# Sigmoid function --------------------------
+
+# Import libraries and data
+import pandas as pd
+import numpy as np
+codecademyU = pd.read_csv('codecademyU.csv')
+
+# Fit the logistic regression model
+hours_studied = codecademyU[['hours_studied']]
+passed_exam = codecademyU[['passed_exam']]
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(hours_studied,passed_exam)
+
+# Save intercept and coef
+intercept = model.intercept_
+coef = model.coef_
+
+# Calculate log_odds here
+log_odds = intercept + coef * hours_studied
+print(log_odds)
+
+# Calculate pred_probability_passing here
+pred_probability_passing = np.exp(log_odds)/(1+np.exp(log_odds))
+print(pred_probability_passing)
