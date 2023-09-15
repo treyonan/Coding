2731bc4185c0cc145ea5664c4cf46412b10caa7f
treyonan <102309885+treyonan@users.noreply.github.com>
2023-09-14 19:56:49 -0500

    amend

--- Python/Code Academy/Machine Learning/Logistic_Regression.py
+++ Python/Code Academy/Machine Learning/Logistic_Regression.py
@@ -187,3 +187,48 @@ print('true classes: ', y_test)
 from sklearn.metrics import confusion_matrix
 print(confusion_matrix(y_test, y_pred))
 
+# Accuracy, Precision, Recall, F1 score --------------
+
+# Import pandas and the data
+import pandas as pd
+codecademyU = pd.read_csv('codecademyU_2.csv')
+
+# Separate out X and y
+X = codecademyU[['hours_studied', 'practice_test']]
+y = codecademyU.passed_exam
+
+# Transform X
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+
+# Split data into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state = 51)
+
+# Create and fit the logistic regression model here:
+from sklearn.linear_model import LogisticRegression
+cc_lr = LogisticRegression()
+cc_lr.fit(X_train,y_train)
+
+# Save and print the predicted outcomes
+y_pred = cc_lr.predict(X_test)
+print('predicted classes: ', y_pred)
+
+# Print out the true outcomes for the test data
+print('true classes: ', y_test)
+
+# Print out the confusion matrix
+from sklearn.metrics import confusion_matrix
+print('confusion matrix: ')
+print(confusion_matrix(y_test, y_pred))
+
+# Print accuracy here:
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
+
+# Print F1 score here:
+from sklearn.metrics import f1_score
+print(f1_score(y_test, y_pred))
+
