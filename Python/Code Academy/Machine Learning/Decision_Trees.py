af24931f56c650c53de254df50a9b6c52b28951d
treyonan <trey_onan@yahoo.com>
2023-10-01 10:54:09 -0500

    amend

---
+++ Python/Code Academy/Machine Learning/Decision_Trees.py
@@ -0,0 +1,47 @@
+# Gini Impurity ---------------------------------------
+
+from collections import Counter
+
+labels = ["unacc", "unacc", "acc", "acc", "good", "good"]
+#labels = ["unacc","unacc","unacc", "good", "vgood", "vgood"]
+#labels = ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc"]
+
+impurity = 1
+label_counts = Counter(labels)
+for label in label_counts:
+  probability_of_label = label_counts[label]/len(labels)
+  impurity -= probability_of_label ** 2
+  
+print(impurity)
+
+# Information Gain ------------------------------------------
+
+from collections import Counter
+
+unsplit_labels = ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc", "good", "good", "good", "good", "vgood", "vgood", "vgood"]
+
+split_labels_1 = [
+  ["unacc", "unacc", "unacc", "unacc", "unacc", "unacc", "good", "good", "vgood"], 
+  [ "good", "good"], 
+  ["vgood", "vgood"]
+]
+
+split_labels_2 = [
+  ["unacc", "unacc", "unacc", "unacc","unacc", "unacc", "good", "good", "good", "good"], 
+  ["vgood", "vgood", "vgood"]
+]
+
+def gini(dataset):
+  impurity = 1
+  label_counts = Counter(dataset)
+  for label in label_counts:
+    prob_of_label = label_counts[label] / len(dataset)
+    impurity -= prob_of_label ** 2
+  return impurity
+
+info_gain = gini(unsplit_labels)
+print(info_gain)
+
+for subset in split_labels_1:
+  info_gain -= gini(subset)
+print(info_gain)
