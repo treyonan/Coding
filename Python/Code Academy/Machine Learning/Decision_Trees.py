51f0e055480e01fee9ee5126daa56bf1afa745fa
treyonan <102309885+treyonan@users.noreply.github.com>
2023-10-10 20:07:13 -0500

    amend

--- Python/Code Academy/Machine Learning/Decision_Trees.py
+++ Python/Code Academy/Machine Learning/Decision_Trees.py
@@ -191,3 +191,14 @@ def build_tree(data, labels):
   
 tree = build_tree(car_data, car_labels)
 print_tree(tree)  
+
+# Decision Trees in sci-kit learn ----------------------
+
+from cars import training_points, training_labels, testing_points, testing_labels
+from sklearn.tree import DecisionTreeClassifier
+
+classifier = DecisionTreeClassifier(random_state = 0, max_depth=11)
+classifier.fit(training_points, training_labels)
+print(classifier.score(testing_points, testing_labels))
+
+print(classifier.tree_.max_depth)
