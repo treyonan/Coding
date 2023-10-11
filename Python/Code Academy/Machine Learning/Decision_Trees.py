98476f05bcf7ca5bc6d6028e20b187965e271a2f
treyonan <102309885+treyonan@users.noreply.github.com>
2023-10-10 19:44:10 -0500

    amend

--- Python/Code Academy/Machine Learning/Decision_Trees.py
+++ Python/Code Academy/Machine Learning/Decision_Trees.py
@@ -45,3 +45,149 @@ print(info_gain)
 for subset in split_labels_1:
   info_gain -= gini(subset)
 print(info_gain)
+
+# Weighted information gain ------------------------
+
+from collections import Counter
+
+cars = [['med', 'low', '3', '4', 'med', 'med'], ['med', 'vhigh', '4', 'more', 'small', 'high'], ['high', 'med', '3', '2', 'med', 'low'], ['med', 'low', '4', '4', 'med', 'low'], ['med', 'low', '5more', '2', 'big', 'med'], ['med', 'med', '2', 'more', 'big', 'high'], ['med', 'med', '2', 'more', 'med', 'med'], ['vhigh', 'vhigh', '2', '2', 'med', 'low'], ['high', 'med', '4', '2', 'big', 'low'], ['low', 'low', '2', '4', 'big', 'med']]
+
+car_labels = ['acc', 'acc', 'unacc', 'unacc', 'unacc', 'vgood', 'acc', 'unacc', 'unacc', 'good']
+
+def split(dataset, labels, column):
+    data_subsets = []
+    label_subsets = []
+    counts = list(set([data[column] for data in dataset]))
+    counts.sort()
+    for k in counts:
+        new_data_subset = []
+        new_label_subset = []
+        for i in range(len(dataset)):
+            if dataset[i][column] == k:
+                new_data_subset.append(dataset[i])
+                new_label_subset.append(labels[i])
+        data_subsets.append(new_data_subset)
+        label_subsets.append(new_label_subset)
+    return data_subsets, label_subsets
+
+def gini(dataset):
+  impurity = 1
+  label_counts = Counter(dataset)
+  for label in label_counts:
+    prob_of_label = label_counts[label] / len(dataset)
+    impurity -= prob_of_label ** 2
+  return impurity
+
+def information_gain(starting_labels, split_labels):
+  info_gain = gini(starting_labels)
+  for subset in split_labels:
+    # Multiply gini(subset) by the correct percentage below
+    info_gain -= gini(subset)*(len(subset)/len(starting_labels))
+  return info_gain
+
+split_data, split_labels = split(cars, car_labels, 3)
+
+#print(split_data)
+#print(len(split_data))
+#print(split_data[1])
+
+#print(information_gain(car_labels, split_labels))
+
+for i in range(len(cars[0])):
+  split_data, split_labels = split(cars, car_labels, i)
+  print(information_gain(car_labels, split_labels))
+
+# Recursive Tree Building-------------------------
+
+# Code below is tree.py
+
+from collections import Counter
+
+def split(dataset, labels, column):
+    data_subsets = []
+    label_subsets = []
+    counts = list(set([data[column] for data in dataset]))
+    counts.sort()
+    for k in counts:
+        new_data_subset = []
+        new_label_subset = []
+        for i in range(len(dataset)):
+            if dataset[i][column] == k:
+                new_data_subset.append(dataset[i])
+                new_label_subset.append(labels[i])
+        data_subsets.append(new_data_subset)
+        label_subsets.append(new_label_subset)
+    return data_subsets, label_subsets
+
+def gini(dataset):
+  impurity = 1
+  label_counts = Counter(dataset)
+  for label in label_counts:
+    prob_of_label = label_counts[label] / len(dataset)
+    impurity -= prob_of_label ** 2
+  return impurity
+
+def information_gain(starting_labels, split_labels):
+  info_gain = gini(starting_labels)
+  for subset in split_labels:
+    info_gain -= gini(subset) * len(subset)/len(starting_labels)
+  return info_gain  
+
+class Leaf:
+    def __init__(self, labels):
+        self.predictions = Counter(labels)
+
+class Internal_Node:
+    def __init__(self,
+                 feature,
+                 branches):
+        self.feature = feature
+        self.branches = branches
+
+def print_tree(node, spacing=""):
+    """World's most elegant tree printing function."""
+    question_dict = {0: "Buying Price", 1:"Price of maintenance", 2:"Number of doors", 3:"Person Capacity", 4:"Size of luggage boot", 5:"Estimated Saftey"}
+    # Base case: we've reached a leaf
+    if isinstance(node, Counter):
+        print (spacing + str(node))
+        return
+
+    # Print the question at this node
+    print (spacing + "Splitting")
+
+    # Call this function recursively on the true branch
+    for i in range(len(node)):
+        print (spacing + '--> Branch ' + str(i)+':')
+        print_tree(node[i], spacing + "  ")
+
+
+
+from tree import *
+
+car_data = [['med', 'low', '3', '4', 'med', 'med'], ['med', 'vhigh', '4', 'more', 'small', 'high'], ['high', 'med', '3', '2', 'med', 'low'], ['med', 'low', '4', '4', 'med', 'low'], ['med', 'low', '5more', '2', 'big', 'med'], ['med', 'med', '2', 'more', 'big', 'high'], ['med', 'med', '2', 'more', 'med', 'med'], ['vhigh', 'vhigh', '2', '2', 'med', 'low'], ['high', 'med', '4', '2', 'big', 'low'], ['low', 'low', '2', '4', 'big', 'med']]
+
+car_labels = ['acc', 'acc', 'unacc', 'unacc', 'unacc', 'vgood', 'acc', 'unacc', 'unacc', 'good']
+
+def find_best_split(dataset, labels):
+    best_gain = 0
+    best_feature = 0
+    for feature in range(len(dataset[0])):
+        data_subsets, label_subsets = split(dataset, labels, feature)
+        gain = information_gain(labels, label_subsets)
+        if gain > best_gain:
+            best_gain, best_feature = gain, feature
+    return best_feature, best_gain
+
+def build_tree(data, labels):
+  best_feature, best_gain = find_best_split(data, labels)
+  if best_gain == 0:
+    return Counter(labels)
+  data_subsets, label_subsets = split(data, labels, best_feature)
+  branches = []
+  for i in range(len(data_subsets)):
+    branch = build_tree(data_subsets[i], label_subsets[i])
+    branches.append(branch)
+  return branches
+  
+tree = build_tree(car_data, car_labels)
+print_tree(tree)  
