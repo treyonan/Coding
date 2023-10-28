89926f9f01b0b4f9a1cf04ec4d6deee3fc2951f9
treyonan <trey_onan@yahoo.com>
2023-10-28 15:54:59 -0500

    amend

--- Python/Code Academy/Machine Learning/Neural_Networks.py
+++ Python/Code Academy/Machine Learning/Neural_Networks.py
@@ -121,3 +121,81 @@ class Perceptron:
 
       if total_error == 0:
         foundLine = True
+
+# Data pre-processing for tensorflow ----------------------------------------------
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+
+#load the dataset
+dataset = pd.read_csv('insurance.csv') 
+#choose first 7 columns as features
+features = dataset.iloc[:,0:6] 
+#choose the final column for prediction
+labels = dataset.iloc[:,-1] 
+
+#one-hot encoding for categorical variables
+features = pd.get_dummies(features) 
+#split the data into training and test data
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) 
+ 
+#normalize the numeric columns using ColumnTransformer
+ct = ColumnTransformer([('normalize', Normalizer(), ['age', 'bmi', 'children'])], remainder='passthrough')
+#fit the normalizer to the training data and convert from numpy arrays to pandas frame
+features_train_norm = ct.fit_transform(features_train) 
+#applied the trained normalizer on the test data and convert from numpy arrays to pandas frame
+features_test_norm = ct.transform(features_test) 
+
+#ColumnTransformer returns numpy arrays. Convert the features to dataframes
+features_train_norm = pd.DataFrame(features_train_norm, columns = features_train.columns)
+features_test_norm = pd.DataFrame(features_test_norm, columns = features_test.columns)
+
+# -----> your code here below
+my_ct = ColumnTransformer([('scale', StandardScaler(), ['age', 'bmi', 'children'])], remainder='passthrough')
+#fit the scaler to the training data and convert from numpy arrays to pandas frame
+features_train_scale = my_ct.fit_transform(features_train) 
+#applied the trained scaler on the test data and convert from numpy arrays to pandas frame
+features_test_scale = my_ct.transform(features_test) 
+
+#ColumnTransformer returns numpy arrays. Convert the features to dataframes
+features_train_scale = pd.DataFrame(features_train_scale, columns = features_train.columns)
+features_test_scale = pd.DataFrame(features_test_scale, columns = features_test.columns)
+
+#print the summary statistics
+print(features_train_scale.describe())
+print(features_test_scale.describe())
+
+# Neural network model: tf.keras.Sequential --------------------------------
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+
+
+def design_model(features):
+  model = Sequential()
+  return model
+  
+dataset = pd.read_csv('insurance.csv') #load the dataset
+features = dataset.iloc[:,0:6] #choose first 7 columns as features
+labels = dataset.iloc[:,-1] #choose the final column for prediction
+
+features = pd.get_dummies(features) #one-hot encoding for categorical variables
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) #split the data into training and test data
+ 
+#standardize
+ct = ColumnTransformer([('standardize', StandardScaler(), ['age', 'bmi', 'children'])], remainder='passthrough')
+features_train = ct.fit_transform(features_train)
+features_test = ct.transform(features_test)
+
+#invoke the function for our model design
+model = design_model(features_train)
+
+#print the layers
+
