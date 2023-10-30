bca0ad4ac233a60241185a28321077767b7cd008
treyonan <trey_onan@yahoo.com>
2023-10-30 16:27:17 -0500

    amend

--- Python/Code Academy/Machine Learning/Neural_Networks.py
+++ Python/Code Academy/Machine Learning/Neural_Networks.py
@@ -198,4 +198,64 @@ features_test = ct.transform(features_test)
 model = design_model(features_train)
 
 #print the layers
+print(model.layers)
 
+# Neural network model: layers -------------------------------
+
+import tensorflow as tf
+from tensorflow.keras import layers
+layer = layers.Dense(3) #3 is the number we chose
+
+print(layer.weights) #we get empty weight and bias arrays because tensorflow doesn't know what the shape is of the input to this layer
+
+# 1338 samples, 11 features as in our dataset
+input = tf.ones((5000, 21)) 
+# a fully-connected layer with 3 neurons
+layer = layers.Dense(10) 
+# calculate the outputs
+output = layer(input) 
+# print the weights
+print(layer.weights) 
+
+# Neural network model: input, output and hidden layers. Optimizers -----------------------------
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+
+def design_model(features):
+  model = Sequential(name = "my_first_model")
+  input = InputLayer(input_shape=(features.shape[1],))
+   #add an input layer
+  model.add(input)
+  #add a hidden layer with 128 neurons
+  model.add(Dense(128, activation='relu')) 
+  #add an output layer
+  model.add(Dense(1)) 
+  #your code here
+  opt = Adam(learning_rate=0.01)
+  model.compile(loss='mse',  metrics=['mae'], optimizer=opt)
+  return model
+
+
+dataset = pd.read_csv('insurance.csv') #load the dataset
+features = dataset.iloc[:,0:6] #choose first 7 columns as features
+labels = dataset.iloc[:,-1] #choose the final column for prediction
+
+features = pd.get_dummies(features) #one-hot encoding for categorical variables
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42) #split the data into training and test data
+ 
+#standardize
+ct = ColumnTransformer([('standardize', StandardScaler(), ['age', 'bmi', 'children'])], remainder='passthrough')
+features_train = ct.fit_transform(features_train)
+features_test = ct.transform(features_test)
+
+#invoke the function for our model design
+model = design_model(features_train)
+print(model.summary())
