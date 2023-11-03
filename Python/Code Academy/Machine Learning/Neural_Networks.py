f726fb309346f7a6d61a4061cbde8f77010ea0cc
treyonan <trey_onan@yahoo.com>
2023-11-02 20:13:47 -0500

    amend

--- Python/Code Academy/Machine Learning/Neural_Networks.py
+++ Python/Code Academy/Machine Learning/Neural_Networks.py
@@ -320,3 +320,337 @@ model = design_model(features_train, learning_rate = 0.01)
 #your code here
 model.fit(features_train, labels_train, epochs = 40, batch_size = 8, verbose = 1, validation_split = 0.33 )
 
+# Manual Tuning: Learning Rate ----------------------------------------------------------------------
+
+#see model.py file for more details
+from model import design_model, features_train, labels_train 
+import matplotlib.pyplot as plt
+
+def fit_model(f_train, l_train, learning_rate, num_epochs, bs):
+    #build the model
+    model = design_model(f_train, learning_rate)
+    #train the model on the training data
+    history = model.fit(f_train, l_train, epochs = num_epochs, batch_size = bs, verbose = 0, validation_split = 0.2)
+    # plot learning curves
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='validation')
+    plt.title('lrate=' + str(learning_rate))
+    plt.legend(loc="upper right")
+
+
+#make a list of learning rates to try out
+learning_rates = [1, 1E-2, 1E-7]
+#fixed number of epochs
+num_epochs = 100
+#fixed number of batches
+batch_size = 10 
+
+for i in range(len(learning_rates)):
+  plot_no = 420 + (i+1)
+  plt.subplot(plot_no)
+  fit_model(features_train, labels_train, learning_rates[i], num_epochs, batch_size)
+
+plt.tight_layout()
+plt.show()
+plt.savefig('static/images/my_plot.png')
+print("See the plot on the right with learning rates", learning_rates)
+import app #don't worry about this. This is to show you the plot in the browser.
+
+# Manual tuning: batch size ---------------------------------
+
+from model import features_train, labels_train, design_model
+import matplotlib.pyplot as plt
+
+def fit_model(f_train, l_train, learning_rate, num_epochs, batch_size, ax):
+    model = design_model(features_train, learning_rate)
+    #train the model on the training data
+    history = model.fit(features_train, labels_train, epochs=num_epochs, batch_size = batch_size, verbose=0, validation_split = 0.3)
+    # plot learning curves
+    ax.plot(history.history['mae'], label='train')
+    ax.plot(history.history['val_mae'], label='validation')
+    ax.set_title('batch = ' + str(batch_size), fontdict={'fontsize': 8, 'fontweight': 'medium'})
+    ax.set_xlabel('# epochs')
+    ax.set_ylabel('mae')
+    ax.legend()
+
+#fixed learning rate 
+learning_rate = 0.1
+#fixed number of epochs
+num_epochs = 100
+#we choose a number of batch sizes to try out
+batches = [4, 32, 64] 
+print("Learning rate fixed to:", learning_rate)
+
+#plotting code
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', sharey='row',
+                        gridspec_kw={'hspace': 0.7, 'wspace': 0.4}) #preparing axes for plotting
+axes = [ax1, ax2, ax3]
+
+#iterate through all the batch values
+for i in range(len(batches)):
+  fit_model(features_train, labels_train, learning_rate, num_epochs, batches[i], axes[i])
+
+plt.savefig('static/images/my_plot.png')
+print("See the plot on the right with batch sizes", batches)
+import app #don't worry about this. This is to show you the plot in the browser.
+
+# Manual tuning: epochs and early stopping --------------------------------------------
+
+from model import features_train, labels_train, design_model
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping
+
+def fit_model(f_train, l_train, learning_rate, num_epochs):
+    #build the model: to see the specs go to model.pyl we increased the number of hidden neurons
+    #in order to introduce some overfitting
+    model = design_model(features_train, learning_rate) 
+    #train the model on the training data
+    #your code here
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
+    history = model.fit(features_train, labels_train, epochs=num_epochs, batch_size= 16, verbose=0, validation_split = 0.2, callbacks = [es])
+    return history
+
+    
+#using the early stopping in fit_model
+learning_rate = 0.1
+num_epochs = 500
+history = fit_model(features_train, labels_train, learning_rate, num_epochs)
+
+#plotting
+fig, axs = plt.subplots(1, 2, gridspec_kw={'hspace': 1, 'wspace': 0.5}) 
+(ax1, ax2) = axs
+ax1.plot(history.history['loss'], label='train')
+ax1.plot(history.history['val_loss'], label='validation')
+ax1.set_title('lrate=' + str(learning_rate))
+ax1.legend(loc="upper right")
+ax1.set_xlabel("# of epochs")
+ax1.set_ylabel("loss (mse)")
+
+ax2.plot(history.history['mae'], label='train')
+ax2.plot(history.history['val_mae'], label='validation')
+ax2.set_title('lrate=' + str(learning_rate))
+ax2.legend(loc="upper right")
+ax2.set_xlabel("# of epochs")
+ax2.set_ylabel("MAE")
+
+print("Final training MAE:", history.history['mae'][-1])
+print("Final validation MAE:", history.history['val_mae'][-1])
+
+plt.savefig('static/images/my_plot.png')
+import app #don't worry about this. This is to show you the plot in the browser.
+
+# Manual tuning: changing the model -----------------------------------
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+from model import features_train, labels_train
+
+def more_complex_model(X, learning_rate):
+    model = Sequential(name="my_first_model")
+    input = tf.keras.Input(shape=(X.shape[1],))
+    model.add(input)
+    model.add(layers.Dense(8, activation='relu'))
+    model.add(layers.Dense(1))
+    opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+    model.compile(loss='mse', metrics=['mae'], optimizer=opt)
+    return model
+
+def one_layer_model(X, learning_rate):
+    model = Sequential(name="my_first_model")
+    input = tf.keras.Input(shape=(X.shape[1],))
+    model.add(input)
+    model.add(layers.Dense(1))
+    opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+    model.compile(loss='mse', metrics=['mae'], optimizer=opt)
+    return model
+
+def fit_model(model, f_train, l_train, learning_rate, num_epochs):
+    #train the model on the training data
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience = 20)
+    history = model.fit(features_train, labels_train, epochs=num_epochs, batch_size= 2, verbose=0, validation_split = 0.2, callbacks = [es])
+    return history
+
+def plot(history):
+    # plot learning curves
+    fig, axs = plt.subplots(1, 2, gridspec_kw={'hspace': 1, 'wspace': 0.8}) 
+    (ax1, ax2) = axs
+    ax1.plot(history.history['loss'], label='train')
+    ax1.plot(history.history['val_loss'], label='validation')
+    ax1.set_title('lrate=' + str(learning_rate))
+    ax1.legend(loc="upper right")
+    ax1.set_xlabel("# of epochs")
+    ax1.set_ylabel("loss (mse)")
+
+    ax2.plot(history.history['mae'], label='train')
+    ax2.plot(history.history['val_mae'], label='validation')
+    ax2.set_title('lrate=' + str(learning_rate))
+    ax2.legend(loc="upper right")
+    ax2.set_xlabel("# of epochs")
+    ax2.set_ylabel("MAE")
+    print("Final training MAE:", history.history['mae'][-1])
+    print("Final validation MAE:", history.history['val_mae'][-1])
+
+learning_rate = 0.1
+num_epochs = 200
+
+#fit the more simple model
+print("Results of a one layer model:")
+history1 = fit_model(one_layer_model(features_train, learning_rate), features_train, labels_train, learning_rate, num_epochs)
+plot(history1)
+plt.savefig('static/images/my_plot1.png')
+
+#fit the more complex model
+print("Results of a model with hidden layers:")
+history2 = fit_model(more_complex_model(features_train, learning_rate), features_train, labels_train, learning_rate, num_epochs)
+plot(history2)
+plt.savefig('static/images/my_plot2.png')
+
+import app #don't worry about this. This is to show you the plot in the browser.
+
+# Towards automated tuning: grid and random search -----------------------------------------
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint as sp_randint
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import make_scorer
+from model import design_model, features_train, labels_train
+
+#------------- GRID SEARCH --------------
+def do_grid_search():
+  batch_size = [6, 64]
+  epochs = [10, 50]
+  model = KerasRegressor(build_fn=design_model)
+  param_grid = dict(batch_size=batch_size, epochs=epochs)
+  grid = GridSearchCV(estimator = model, param_grid=param_grid, scoring = make_scorer(mean_squared_error, greater_is_better=False),return_train_score = True)
+  grid_result = grid.fit(features_train, labels_train, verbose = 0)
+  print(grid_result)
+  print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+  means = grid_result.cv_results_['mean_test_score']
+  stds = grid_result.cv_results_['std_test_score']
+  params = grid_result.cv_results_['params']
+  for mean, stdev, param in zip(means, stds, params):
+      print("%f (%f) with: %r" % (mean, stdev, param))
+
+  print("Traininig")
+  means = grid_result.cv_results_['mean_train_score']
+  stds = grid_result.cv_results_['std_train_score']
+  for mean, stdev, param in zip(means, stds, params):
+      print("%f (%f) with: %r" % (mean, stdev, param))
+
+#------------- RANDOMIZED SEARCH --------------
+def do_randomized_search():
+  param_grid = {'batch_size': sp_randint(2, 16), 'nb_epoch': sp_randint(10, 100)}
+  model = KerasRegressor(build_fn=design_model)
+  grid = RandomizedSearchCV(estimator = model, param_distributions=param_grid, scoring = make_scorer(mean_squared_error, greater_is_better=False), n_iter = 12)
+  grid_result = grid.fit(features_train, labels_train, verbose = 0)
+  print(grid_result)
+  print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+  means = grid_result.cv_results_['mean_test_score']
+  stds = grid_result.cv_results_['std_test_score']
+  params = grid_result.cv_results_['params']
+  for mean, stdev, param in zip(means, stds, params):
+      print("%f (%f) with: %r" % (mean, stdev, param))
+
+print("-------------- GRID SEARCH --------------------")
+do_grid_search()
+print("-------------- RANDOMIZED SEARCH --------------------")
+do_randomized_search()
+
+# Regularization: dropout -------------------------------
+
+rom model import features_train, labels_train, fit_model
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import EarlyStopping
+from plotting import plot
+import matplotlib.pyplot as plt
+
+def design_model_dropout(X, learning_rate):
+    model = Sequential(name="my_first_model")
+    input = tf.keras.Input(shape=(X.shape[1],))
+    model.add(input)
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.1))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(24, activation='relu'))
+    #------your code here!------
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Dense(1))
+    opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+    model.compile(loss='mse', metrics=['mae'], optimizer=opt)
+    return model
+
+def design_model_no_dropout(X, learning_rate):
+    model = Sequential(name="my_first_model")
+    input = layers.InputLayer(input_shape=(X.shape[1],))
+    model.add(input)
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(24, activation='relu'))
+    model.add(layers.Dense(1))
+    opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+    model.compile(loss='mse', metrics=['mae'], optimizer=opt)
+    return model
+    
+#using the early stopping in fit_model
+learning_rate = 0.001
+num_epochs = 200
+#train the model without dropout
+history1 = fit_model(design_model_no_dropout(features_train, learning_rate), features_train, labels_train, learning_rate, num_epochs)
+#train the model with dropout
+history2 = fit_model(design_model_dropout(features_train, learning_rate), features_train, labels_train, learning_rate, num_epochs)
+
+plot(history1, 'static/images/no_dropout.png')
+
+plot(history2, 'static/images/with_dropout.png')
+
+import app #don't worry about this. This is to show you the plot in the browser.
+
+# Baselines: how to know the performance is reasonable? --------------------------
+
+#see model.py file for more details
+from model import features_train, labels_train, features_test, labels_test
+import matplotlib.pyplot as plt
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import mean_absolute_error
+
+
+dummy_regr = DummyRegressor(strategy="median")
+dummy_regr.fit(features_train, labels_train)
+y_pred = dummy_regr.predict(features_test)
+MAE_baseline = mean_absolute_error(labels_test, y_pred)
+print(MAE_baseline)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
