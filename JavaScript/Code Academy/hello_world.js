6d207e56745e731eca607e73d43e1e35b7ad2e28
treyonan <trey_onan@yahoo.com>
2024-07-14 17:33:05 -0500

    amend

--- JavaScript/Code Academy/hello_world.js
+++ JavaScript/Code Academy/hello_world.js
@@ -6,3 +6,13 @@ multi line comment
 */
 
 console.log(11 % 3);
+console.log('hiyah'.length);
+
+console.log('Hello' + 'World');
+
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace   '.trim());
+
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
