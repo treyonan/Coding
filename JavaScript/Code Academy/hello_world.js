fb65510345a88a1a6c9b324a3af52b6f6c07ff64
treyonan <trey_onan@yahoo.com>
2024-07-23 21:42:04 -0500

    amend

--- JavaScript/Code Academy/hello_world.js
+++ JavaScript/Code Academy/hello_world.js
@@ -28,10 +28,6 @@ powerLevel -= 100;
 multiplyMe *= 11;
 quarterMe /= 4;
 
-
-
-
-
 // These console.log() statements below will help you check the values of the variables.
 // You do not need to edit these statements. 
 console.log('The value of levelUp:', levelUp); 
@@ -98,4 +94,63 @@ switch(athleteFinalPosition){
   default:
     console.log('No medal awarded.');
     break;
+}
+
+// Magic 8 Ball Project
+
+let userName = 'Trey';
+userName !== '' ? console.log(`Hello, ${userName}`) : console.log('Hello')
+let userQuestion = 'Will I be rich someday?';
+console.log(`${userName} asked ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch(randomNumber){
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say so';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;  
+  default:
+    eightBall = 'blah';
+    break;
+}
+console.log(eightBall);
+
+// Race Day Project
+
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly = false;
+let age = 18;
+if (age > 18 && registeredEarly) {
+  raceNumber += 1000;
+}
+if (age > 18 && registeredEarly) {
+  console.log(`You will race at 9:30 am, your race number is ${raceNumber}`);
+} else if (age > 18 && !registeredEarly) {
+  console.log(`You will race at 11:00 am, your race number is ${raceNumber}`);
+}
+if (age < 18) {
+  console.log(`You will race at 12:30 pm, your race number is ${raceNumber}`);
+}
+if (age === 18) {
+  console.log(`See registration desk`);
 }
