791dcbcd694ca3170cc5d3a6a933d89ea580e198
treyonan <trey_onan@yahoo.com>
2024-08-02 20:57:53 -0500

    amend

--- JavaScript/Code Academy/functions.js
+++ JavaScript/Code Academy/functions.js
@@ -3,3 +3,63 @@ function sayThanks(name) {
   }
   
   sayThanks('Cole');
+
+  // Default parameters
+
+  function greeting (name = 'stranger') {
+    console.log(`Hello, ${name}!`)
+  }
+  
+  greeting('Nick') // Output: Hello, Nick!
+  greeting() // Output: Hello, stranger!
+
+  // Return values
+
+  function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  const numOfMonitors = monitorCount(5, 4);
+  
+  console.log(numOfMonitors);
+
+  //------------------------------------------------
+
+  function monitorCount(rows, columns) {
+    return rows * columns;
+  }
+  
+  function costOfMonitors(rows, columns) {
+    return monitorCount(rows, columns) * 200;
+  }
+  
+  const totalCost = costOfMonitors(5, 4);
+  
+  console.log(totalCost);
+
+  // Function Expressions
+
+  const plantNeedsWater = function(day) {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  console.log(plantNeedsWater('Tuesday'));
+
+  // Arrow Functions
+
+  const plantNeedsWater = (day) => {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Concise Body Arrow Functions
+
+  const plantNeedsWater = day => day === 'Wednesday' ? true : false;
+  
