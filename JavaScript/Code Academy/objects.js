d3883a357a5a6edaf506d2ee9fbb418080f46e11
treyonan <trey_onan@yahoo.com>
2024-08-25 09:48:02 -0500

    amend

--- JavaScript/Code Academy/objects.js
+++ JavaScript/Code Academy/objects.js
@@ -17,18 +17,247 @@ let crewCount = spaceship.numCrew;
 let planetArray = spaceship.flightPath;
 
 let returnAnyProp = (objectName, propName) => objectName[propName];
- 
+
 returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
 
 let spaceship = {
-    'Fuel Type' : 'Turbo Fuel',
-    'Active Mission' : true,
-    homePlanet : 'Earth', 
+    'Fuel Type': 'Turbo Fuel',
+    'Active Mission': true,
+    homePlanet: 'Earth',
     numCrew: 5
-   };
+};
+
+let propName = 'Active Mission';
+
+let isActive = spaceship['Active Mission'];
+console.log(isActive);
+
+// Property assignment
+
+let spaceship = {
+    'Fuel Type': 'Turbo Fuel',
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Secret Mission': 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 6;
+delete spaceship['Secret Mission'];
+
+// Methods
+
+const alienShip = {
+    invade() {
+        console.log('Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.')
+    }
+};
+
+alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+const alienShip = {
+    retreat() {
+        console.log(retreatMessage);
+    },
+    takeOff() {
+        console.log('Spim... Borp... Glix... Blastoff!');
+    }
+};
+
+alienShip.retreat();
+alienShip.takeOff();
+
+// Nested Objects
+
+const spaceship = {
+    telescope: {
+       yearBuilt: 2018,
+       model: '91031-XLT',
+       focalLength: 2032 
+    },
+   crew: {
+       captain: { 
+           name: 'Sandra', 
+           degree: 'Computer Engineering', 
+           encourageTeam() { console.log('We got this!') } 
+        }
+   },
+   engine: {
+       model: 'Nimbus2000'
+    },
+    nanoelectronics: {
+        computer: {
+           terabytes: 100,
+           monitors: 'HD'
+        },
+       'back-up': {
+          battery: 'Lithium',
+          terabytes: 50
+        }
+   }
+}; 
+
+spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
+
+
+
+let spaceship = {
+    passengers: [{name: 'Space Dog'}], 
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
   
-  let propName =  'Active Mission';
+  let capFave = spaceship.crew.captain['favorite foods'][0];
   
-  let isActive = spaceship['Active Mission'];
-  console.log(isActive);
+  let firstPassenger = spaceship.passengers[0];
+
+  // Pass By Reference
+
+  const spaceship = {
+    homePlanet : 'Earth',
+    color : 'silver'
+  };
+   
+  let paintIt = obj => {
+    obj.color = 'glorious gold'
+  };
+   
+  paintIt(spaceship);
+   
+  spaceship.color // Returns 'glorious gold'
+
+
+
+  let spaceship = {
+    homePlanet : 'Earth',
+    color : 'red'
+  };
+  let tryReassignment = obj => {
+    obj = {
+      identified : false, 
+      'transport type' : 'flying'
+    }
+    console.log(obj) // Prints {'identified': false, 'transport type': 'flying'}
+   
+  };
+  tryReassignment(spaceship) // The attempt at reassignment does not work.
+  spaceship // Still returns {homePlanet : 'Earth', color : 'red'};
+   
+  spaceship = {
+    identified : false, 
+    'transport type': 'flying'
+  }; // Regular reassignment still works.
+
+
+  let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+  };
   
+  // Write your code below
+  
+  let greenEnergy = obj => {
+    obj['Fuel Type'] = 'avocado oil';
+  }
+  
+  let remotelyDisable = obj => {
+    obj.disabled = true;
+  }
+  
+  greenEnergy(spaceship);
+  
+  remotelyDisable(spaceship);
+  
+  console.log(spaceship)
+
+  // Looping Through Objects
+
+  let spaceship = {
+    crew: {
+      captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+      },
+      'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+      },
+      medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+      translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+      }
+    }
+  }; 
+  
+  // for...in
+  for (let crewMember in spaceship.crew) {
+    console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+  }
+
+
+
+  let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
+
+
