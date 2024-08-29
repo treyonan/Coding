529f2797b84ff42e24b4e0a76fcc592a5c14a5ec
treyonan <trey_onan@yahoo.com>
2024-08-28 21:54:19 -0500

    amend

--- JavaScript/Code Academy/advanced_objects.js
+++ JavaScript/Code Academy/advanced_objects.js
@@ -200,3 +200,36 @@ console.log(robotEntries);
 const newRobot = Object.assign({ laserBlaster: true, voiceRecognition: true }, robot);
 
 console.log(newRobot);
+
+
+// Meal Maker Project
+
+const menu = {
+    _meal: '',
+    _price: 0,
+
+    set meal(mealToCheck) {
+        if (typeof mealToCheck === 'string') {
+            return this._meal = mealToCheck;
+        }
+    },
+
+    set price(priceToCheck) {
+        if (typeof priceToCheck === 'number') {
+            return this._price = priceToCheck;
+        }
+    },
+
+    get todaySpecial() {
+        if (this._meal && this._price) {
+            return `Today's Meal is ${this._meal} for ${this._price}!`;
+        } else {
+            return `Meal or price not set correctly!`;
+        }
+    }
+};
+
+menu.meal = 'Pizza';
+menu.price = 8;
+console.log(menu.todaySpecial);
+
