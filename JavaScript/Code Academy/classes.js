955eebb7d85bf1a7e78102a976b4d711f836c7d7
treyonan <trey_onan@yahoo.com>
2024-09-06 21:55:04 -0500

    amend

--- JavaScript/Code Academy/classes.js
+++ JavaScript/Code Academy/classes.js
@@ -248,3 +248,77 @@ speed.addRating(1);
 speed.addRating(5);
 console.log(speed.getAverageRating());
 
+
+// School Catalogue Project
+
+class School {
+
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    set numberOfStudents(value) {
+        if (value.isNaN()) {
+            console.log('Invalid input: numberOfStudents must be set to a Number.')
+        }
+        else {
+            this._numerOfStudents = value;
+        }
+    }
+
+    quickFacts() {
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
+    }
+
+    static pickStubstituteTeacher(substituteTeachers) {
+        const randInt = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[randInt];
+    }
+}
+
+class PrimarySchool extends School {
+
+    constructor(name, numberOfStudents, pickupPolicy) {
+        super(name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+}
+
+class HighSchool extends School {
+
+    constructor(name, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+
+    get sportsTeams() {
+        return this._sportsTeams;
+    }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member of the age of 13');
+lorraineHansbury.quickFacts();
+
+const sub = School.pickStubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J.R. Smith', 'James Harden']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith.sportsTeams);
