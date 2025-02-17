639e2f63bf235bd34eda7781423e0203cd043d17
treyonan <trey_onan@yahoo.com>
2025-02-16 19:25:25 -0600

    amend

--- JavaScript/Code Academy/requests.js
+++ JavaScript/Code Academy/requests.js
@@ -151,3 +151,205 @@ const renderJsonResponse = (res) => {
 
 // POST Requests using Fetch ----------------------------------------------------------------------------------------------------
 
+// Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'apikey': apiKey
+        },
+        body: data
+    })
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+// Handling a POST Request ---------------------------------------------------------------------------------------------------------
+
+/ Information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'apikey': apiKey
+        },
+        body: data
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message);
+    }).then(jsonResponse => {
+        renderResponse(jsonResponse);
+    })
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
+// Helper function
+
+// Manipulates responseField to render a formatted and appropriate message
+const renderResponse = (res) => {
+    // Displays either message depending on results
+    if (res.errors) {
+        responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {
+        responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+    }
+}
+
+// Manipulates responseField to render an unformatted response
+const renderRawResponse = (res) => {
+    // Displays either message depending on results
+    if (res.errors) {
+        responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {
+        // Adds line breaks for JSON
+        let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+        structuredRes = `<pre>${structuredRes}</pre>`;
+        responseField.innerHTML = `${structuredRes}`;
+    }
+}
+
+// Renders the JSON that was returned when the Promise from fetch resolves.
+const renderJsonResponse = (response) => {
+    // Creates an empty object to store the JSON in key-value pairs
+    let rawJson = {}
+    for (let key in response) {
+        rawJson[key] = response[key]
+    }
+    // Converts JSON into a string and adding line breaks to make it easier to read
+    rawJson = JSON.stringify(rawJson).replace(/,/g, ", \n")
+    // Manipulates responseField to show the returned JSON.
+    responseField.innerHTML = `<pre>${rawJson}</pre>`
+}
+
+
+// Async GET Request ----------------------------------------------------------------------------------
+
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous function
+// Code goes here
+const getSuggestions = async () => {
+    const wordQuery = inputField.value;
+    const endpoint = `${url}${queryParams}${wordQuery}`;
+    try {
+        const response = await fetch(endpoint, { cache: 'no-cache' });
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            renderResponse(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+// Async POST Request ----------------------------------------------------------------------------------
+
+// information to reach API
+const apiKey = '<Your API Key>';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+// Code goes here
+const shortenUrl = async () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Content-type': 'application/json',
+                'apikey': apiKey
+            }
+        });
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            renderResponse(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
