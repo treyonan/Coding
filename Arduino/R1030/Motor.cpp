f56ca2e0fa7384b249903075b28cfdc23491a28a
treyonan <trey_onan@yahoo.com>
2025-08-17 12:05:55 -0500

    amend

--- Arduino/R1030/Motor.cpp
+++
@@ -1,9 +0,0 @@
-#include "Motor.h"
-
-void Motor::Arm(int signalLevel, int delayTime) {
-  writeMicroseconds(signalLevel);  // send "stop" signal to Motor ESC
-  delay(delayTime);                // delay to allow the Motor ESC to recognize the stopped signal
-}
-void Motor::Resume(int signalLevel) {
-  writeMicroseconds(signalLevel);
-}
